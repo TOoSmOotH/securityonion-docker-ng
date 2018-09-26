93195f5911a687208df53810bd56b80ea4c94bfa
Mike Reeves <luke@geekempire.com>
2018-09-25 21:07:22 -0400

    Steno Docker - Change shell for steno and permissions

--- so-steno/files/so-steno.sh
+++ so-steno/files/so-steno.sh
@@ -2,5 +2,7 @@
 
 # Generate the keys if they have not been already
 /usr/bin/stenokeys.sh 941 941
-setcap 'CAP_NET_RAW+ep CAP_NET_ADMIN+ep CAP_IPC_LOCK+ep' /usr/bin/stenotype
-/usr/bin/stenographer -syslog=false
+
+chown -R 941:941 /etc/stenographer/certs
+
+runuser -u stenographer -c '/usr/bin/stenographer -syslog=false >> /var/log/stenographer/stenographer.log 2>&1'
