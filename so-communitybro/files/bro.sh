c4640c49dedf27d8880ace475f58fed0ab524354
Mike Reeves <luke@geekempire.com>
2018-10-01 19:45:45 -0400

    Community Bro Docker - fix capadd

--- so-communitybro/files/bro.sh
+++ so-communitybro/files/bro.sh
@@ -2,7 +2,5 @@
 
 setcap cap_net_raw,cap_net_admin=eip /opt/bro/bin/bro
 setcap cap_net_raw,cap_net_admin=eip /opt/bro/bin/capstats
-chown -R 937:937 /opt/bro
-chown -R 937:937 /nsm/bro
 runuser bro -c '/opt/bro/bin/broctl deploy'
 sleep infinity
